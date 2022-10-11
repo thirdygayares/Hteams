27c9353878f30cf616df1eeb1bd9f6554eb11d55
ZYD-9 <novemjanydzel@gmail.com>
2022-10-11 10:09:51 +0800

    testing connection

--- app/src/main/java/com/example/hteams/MainActivity.java
+++ app/src/main/java/com/example/hteams/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.hteams;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputLayout input;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = findViewById(R.id.input);
+        button = findViewById(R.id.enter);
+
+        FirebaseFirestore myDB;
+        // Init FireStore
+        myDB = FirebaseFirestore.getInstance();
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //insert data
+                Map<String, Object> data = new HashMap<>();
+                data.put("task_name", input.getEditText().toString());
+                myDB.collection("tasks")
+                        .add(data)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+
+                                Toast.makeText(getApplicationContext(),"Data Sucessfully inseted", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(),"Error while adding the data : " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }
+        });
+
+
     }
 }
