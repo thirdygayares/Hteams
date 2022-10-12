d11a18931de977da57695dd64ab9863066eee3e1
Novem <novemjanydzel@gmail.com>
2022-10-12 02:48:31 +0000

    creategroup class created inserted assets in the drawable folder

---
+++ app/src/main/java/com/example/hteams/creategroup.java
@@ -0,0 +1,23 @@
+package com.example.hteams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class creategroup extends AppCompatActivity {
+
+    Button backbtn;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creategroup);
+
+
+    }
+}
