77914e4d8f7e096f1ab6e6e9c01350c6d243c73d
Novem <novemjanydzel@gmail.com>
2022-10-13 04:02:08 +0000

    user profile created xml only

---
+++ app/src/main/java/com/example/hteams/userprofile.java
@@ -0,0 +1,16 @@
+package com.example.hteams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userprofile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userprofile);
+
+
+    }
+}
