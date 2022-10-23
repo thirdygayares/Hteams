17017723b3b4a7b8d4f9573e69853d7485afcda3
Novem <novemjanydzel@gmail.com>
2022-10-23 00:36:58 +0000

    bottom sheet done note:to be tested from other emulator.

--- app/src/main/java/com/example/hteams/Comments.java
+++ app/src/main/java/com/example/hteams/Comments.java
@@ -2,13 +2,58 @@ package com.example.hteams;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.nio.InvalidMarkException;
 
 public class Comments extends AppCompatActivity {
+
+    BottomSheetDialog linkdialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
+
+
+        ImageButton links = findViewById(R.id.linkbtn);
+        linkdialog = new BottomSheetDialog(this);
+        createDialog();
+
+          links.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  linkdialog.show();
+              }
+          });
+    }
+
+    private void createDialog() {
+
+        View view = getLayoutInflater().inflate(R.layout.bottomsheet,null,false);
+        Button submit = findViewById(R.id.submitbtn);
+        EditText site = findViewById(R.id.sitenamefield);
+        EditText name = findViewById(R.id.customenamefield);
+        EditText sitelink = findViewById(R.id.linkfield);
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linkdialog.dismiss();
+                Toast.makeText(Comments.this,name.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        linkdialog.setContentView(view);
     }
 }
